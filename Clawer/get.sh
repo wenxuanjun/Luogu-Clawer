d7b15a8eae2399bc8722b3a81cc144a14f5d25c3
wendster <wendster@163.com>
2018-11-05 19:41:24 +0800

    Make a nice get.sh

--- Clawer/get.sh
+++ Clawer/get.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
+#我是不是应该在这里写一些胡乱的东西来假装Shell写了很多呢？
+
+#也许会很有用哦OvO
+
+#皮皮真开心  ——鲁迅
+
 mkdir Luogu
 for ((which = 1000; which <= 4954; i++)); do wget https://www.luogu.org/problemnew/show/P$which -O Luogu/$which.htm; done
